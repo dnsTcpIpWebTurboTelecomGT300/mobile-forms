bbd88802b5152a363d1be36ad080c4ed12da02e8
ToshaBritva <mineloveguitar@gmail.com>
2016-10-26 00:17:11 +0500

    Added script for collections

---
+++ Projects/odata-server/db/collections.js
@@ -0,0 +1,58 @@
+    db.createCollection ( "users", {
+        validator: {
+            authType: { $type: "string" },
+            externalId: { $exists: true  },
+            firstName: { $type: "string" },
+            lastName: { $type: "string" },
+            avatar: { $type: "string" }
+        },
+        validationAction: "error",
+        validationLevel: "strict"
+    });
+    db.users.createIndex( { "externalId": 1 }, { unique: true } );
+
+    db.createCollection( "quizes", { 
+    	validator: { 
+            userId: { $type: "string" },
+            name: { $type: "string" },
+            description: { $type: "string" },
+            creationDate: { $type: "date"},
+            isPublished: { $type: "bool" }
+        },
+       validationAction: "error",
+       validationLevel: "strict"
+    });
+
+    db.createCollection( "questions", {
+    	validator: {
+    		quizId: { $type: "string" },
+    		text: { $type: "string"},
+    		isRequired : { $type: "bool"},
+            questionType: { $type: "string" },
+    		$or: [
+                { 
+                    answerFormat: { $type: "string" }
+                },
+                { 
+                    $and: [
+                        { questionType: { $type: "string" } },
+                        { isMulti: {$type: "bool"} },
+                        { "variants.0": { $exists: true } } 
+                    ]
+                }
+            ]
+    	},
+    	validationAction: "error",
+    	validationLevel: "strict"
+    });
+
+    db.createCollection("userAnswers", {
+    	validator: {
+    		quizId: { $type: "string" },
+    		userId: { $type: "string"},
+    		date : { $type: "date"},
+            "answers.0": { $exists: true }
+    	},
+    	validationAction: "error",
+    	validationLevel: "strict"
+    });
