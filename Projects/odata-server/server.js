65dbb5594cc299abbf717725fc9be2161efff241
BaR488 <boris_traktor@mail.ru>
2016-12-19 01:20:46 +0500

    Fix upload size limit

--- Projects/odata-server/server.js
+++ Projects/odata-server/server.js
@@ -1,12 +1,21 @@
 var configUtils = require('./lib/configUtils');
 var config = require('./lib/config')
-var odata = require('node-odata');
+var odata = require('./node-odata');
 var logger = require('morgan');
+var _bodyParser = require('./node_modules/node-odata/node_modules/body-parser');
+
+var _bodyParser2 = _interopRequireDefault(_bodyParser);
+
+function _interopRequireDefault(obj) {
+    return obj && obj.__esModule ? obj : {default: obj};
+}
+
+
 
 console.log("Starting OData server");
 console.log("Connecting to database: " + configUtils.getMongoConnectionString());
 
-var server = odata(configUtils.getMongoConnectionString());
+var server = odata.default(configUtils.getMongoConnectionString());
 server.set("prefix", config.get("apiPrefix"));
 server.use(logger('dev'));
 
